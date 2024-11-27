f5519178702fce20d48dccf76b624ae7297f2056
Juan Garcia <juan.garcia16@utp.edu.co>
2024-11-26 19:11:21 -0500

    Eliminacion archivos js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 console.log("que se dice");
 import './AnimarGaleria.js' 
-import './MostrarTrabajo.js'
 import './slider.js' 
 import './scripts.js'
- /* import './arreglo.js' */
- import {AnimarTexto}  from './AnimarTexto.js' 
-/* import './MostrarFooter.js' */
+import {AnimarTexto}  from './AnimarTexto.js' 
 
 
 window.addEventListener("load",async(e)=>{
